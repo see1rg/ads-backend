39c863c7f4d71d64c9b66e53397c1dd9147b4295
see1rg <see1rg@mail.ru>
2023-05-27 12:48:51 +0300

    исправил замечания ПР

--- src/main/java/ru/skypro/homework/models/Image.java
+++ src/main/java/ru/skypro/homework/models/Image.java
@@ -21,7 +21,7 @@ public class Image {
     private String mediaType;
     @Lob
     private byte[] preview;
-    @ManyToOne
+    @OneToOne
     @JoinColumn(name = "ads_id", referencedColumnName = "id")
     private Ads ads;
 
